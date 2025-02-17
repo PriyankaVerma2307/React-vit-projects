bf49ad5479bd4f3a04e02855d2dfae97902d801e
PriyankaVerma2307 <vermapriyanka32892@gmail.com>
2025-02-17 13:42:39 +0530

    main

---
+++ 03reactrouter/src/components/Github/Github.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    
+  return (
+    <div>
+      <h1 className="bg-slate-600 text-white text-5xl p-10">Github</h1>
+    </div>
+  )
+}
+
+export default Github
+
